7b8e633fe7277264e8d940c914b6169f75c92df7
patresio <patresio@gmail.com>
2023-11-08 10:31:23 -0300

    fix: error input.css colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,39 +21,15 @@ module.exports = {
       //     "linear-gradient(45deg, #5033C3 0%, rgba(80, 51, 195, 0.20) 100%);",
       // },
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        main: "hsl(var(--main-color))",
+        personred: "hsl(var(--red))",
+        personyellow: "hsl(var(--yellow))",
+        personcornflower: "hsl(var(--corn-flower))",
+        personcream: "hsl(var(--cream))",
       },
       borderRadius: {
         lg: "var(--radius)",
