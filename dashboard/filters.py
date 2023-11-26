96b862ee0b463b5e82708dd18262c8c71cae0ffa
patresio <patresio@gmail.com>
2023-11-26 17:15:50 -0300

    fix: additional css

--- dashboard/filters.py
+++ dashboard/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.forms import TextInput
 from django_filters import CharFilter
 
 from dashboard.models import Direction, Sector
@@ -8,8 +9,10 @@ from dashboard.models import Direction, Sector
 ############################ SETORES E DIRETORIAS ############################################
 ##############################################################################################
 class DirectionFilter(django_filters.FilterSet):
-    name = CharFilter(field_name="name", lookup_expr="icontains")
-    accountable = CharFilter(field_name="accountable", lookup_expr="icontains")
+    name = CharFilter(label="Pesquise por nome:", field_name="name", lookup_expr="icontains", widget=TextInput(attrs={
+                      "class": "block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}))
+    accountable = CharFilter(field_name="accountable", lookup_expr="icontains", widget=TextInput(attrs={
+        "class": "block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}))
 
     class Meta:
         model = Direction
@@ -28,6 +31,8 @@ class SectorFilter(django_filters.FilterSet):
 ##############################################################################################
 ########################### LICITAÇÃO E SUPRIMENTOS ##########################################
 ##############################################################################################
+
+
 class MaterialFilter(django_filters.FilterSet):
     name = CharFilter(field_name="name", lookup_expr="icontains")
 
--- dashboard/forms.py
+++ dashboard/forms.py
@@ -22,11 +22,17 @@ class DirectionForm(forms.ModelForm):
         words = [w.capitalize() for w in name.split()]
         return " ".join(words)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+
 
 class SectorForm(forms.ModelForm):
     class Meta:
         model = Sector
-        fields = ["name", "accountable", "direction", "phone", "email", "address"]
+        fields = ["name", "accountable",
+                  "direction", "phone", "email", "address"]
 
     def clean_name(self):
         return self.extract_from_clean("name")
@@ -42,7 +48,8 @@ class SectorForm(forms.ModelForm):
     def clean(self):
         self.cleaned_data = super().clean()
         if not self.cleaned_data.get("email") and not self.cleaned_data.get("phone"):
-            raise forms.ValidationError("Informe ao menos um e-mail ou telefone.")
+            raise forms.ValidationError(
+                "Informe ao menos um e-mail ou telefone.")
         return self.cleaned_data
 
 
