a565fb582c70e7122d25857bd3dc3aec3b2c75dd
Ricardo Pereira <patresio@gmail.com>
2023-11-28 15:24:18 -0300

    deploy: try 4

--- core/settings.py
+++ core/settings.py
@@ -138,7 +138,7 @@ MEDIA_ROOT = BASE_DIR / 'media' / 'images'
 
 COMPRESS_ROOT = BASE_DIR / 'static'
 COMPRESS_ENABLED = True
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+# STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("auth/", include("authenticate.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
