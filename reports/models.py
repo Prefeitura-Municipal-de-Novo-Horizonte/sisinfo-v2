62b223b03b0862ed81f24a556921a1332a18c236
Ricardo Pereira <patresio@gmail.com>
2023-12-01 15:31:35 -0300

    :construction: Construindo o models do laudos

--- reports/models.py
+++ reports/models.py
@@ -1,5 +1,14 @@
+from random import choices
+
 from django.db import models
+
 
 # Create your models here.
 class Reports(models.Model):
-    
+    KINDS = (('1', 'Aguardando ...'), ('2', 'Finalizado'))
+    number_report = models.CharField(
+        'identificação do laudo', max_length=20, unique=True, blank=True, null=True)
+    sector = models.ForeignKey(
+        'Sector', verbose_name='setor', on_delete=models.SET_NULL, blank=True, null=True)
+    employee = models.CharField('funcionario', max_length=200, blank=True)
+    status = models.Charfield('status', max_lenght=1, default=1, choices=KINDS)
