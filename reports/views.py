74e45be9c64ec2ed2e0ba57afdbb0b91ce8ba853
Ricardo Pereira <patresio@gmail.com>
2023-12-06 09:20:20 -0300

    :construction: feat: pagination in the reports ok

--- reports/views.py
+++ reports/views.py
@@ -1,15 +1,25 @@
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.core.paginator import Paginator
 from django.forms import inlineformset_factory
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from dashboard.models import Material
 from reports.forms import MaterialReportForm, ReportForm
 from reports.models import MaterialReport, Report
 
 
 # Create your views here.
 def reports(request):
-    context = {}
+    report_list = Report.objects.all()
+    paginator = Paginator(report_list, 15)  # Show 15 reports per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "page_obj": page_obj,
+    }
     return render(request, 'reports.html', context)
 
 
@@ -24,7 +34,9 @@ def report_register(request):
             report = form.save()
             form_material.instance = report
             form_material.save()
-            return HttpResponse('Sucessfully registered')
+            messages.add_message(request, constants.SUCCESS,
+                                 f'Laudo {report.slug} salvo com sucesso!')
+            return redirect(reverse('reports:register_report'))
         else:
             return HttpResponse('Failed to register')
     form = ReportForm(request=request)
