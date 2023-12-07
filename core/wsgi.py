89b698f12f8e46607b24526cf77a3227411d430d
Ricardo Pereira <patresio@gmail.com>
2023-12-07 15:46:40 -0300

    fix error ok

--- core/wsgi.py
+++ core/wsgi.py
@@ -10,12 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from email.mime import application
 
-# from dj_static import Cling
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-# application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
 
 application = get_wsgi_application()
 
