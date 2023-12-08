a23d6059688b0c9b50892157104a33d825658efe
Ricardo Pereira <patresio@gmail.com>
2023-12-08 07:53:49 -0300

    fix: error wsgi serverless crashed

--- core/asgi.py
+++ core/asgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from dj_static import Cling
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-application = get_asgi_application()
+application = Cling(get_asgi_application())
+
+app = application
--- core/wsgi.py
+++ core/wsgi.py
@@ -17,6 +17,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = Cling(get_wsgi_application())
 
-application = get_wsgi_application()
-
 app = application
