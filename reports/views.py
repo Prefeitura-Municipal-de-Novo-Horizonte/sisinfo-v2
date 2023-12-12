48353a929f8f2bf8e73ffba3d41eb49b1b799e09
Ricardo Pereira <patresio@gmail.com>
2023-12-12 07:18:39 -0300

    :bug: fix: error bug in the weasyprint test for generate reports

--- reports/views.py
+++ reports/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 # from django.template.loader import get_template
 from django.urls import reverse
+from weasyprint import CSS, HTML
 
 from reports.forms import MaterialReportForm, MaterialReportFormset, ReportForm, ReportUpdateForm
 from reports.models import MaterialReport, Report
